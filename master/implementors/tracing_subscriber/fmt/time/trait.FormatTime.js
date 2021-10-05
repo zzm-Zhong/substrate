fb9bd6b602c90fdcc33354068288d82c6ceb42b2
paritytech-ci <devops-team@parity.io>
2021-10-05 13:02:56 +0000

    ___Updated docs for master___

---
+++ master/implementors/tracing_subscriber/fmt/time/trait.FormatTime.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["sc_tracing"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/tracing-subscriber/0.2.19/tracing_subscriber/fmt/time/trait.FormatTime.html\" title=\"trait tracing_subscriber::fmt::time::FormatTime\">FormatTime</a> for <a class=\"struct\" href=\"sc_tracing/logging/struct.FastLocalTime.html\" title=\"struct sc_tracing::logging::FastLocalTime\">FastLocalTime</a>","synthetic":false,"types":["sc_tracing::logging::fast_local_time::FastLocalTime"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
