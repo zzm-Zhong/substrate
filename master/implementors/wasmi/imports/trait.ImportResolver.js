11299e91b1dd158106dadf64d3d35b91f3076c29
paritytech-ci <devops-team@parity.io>
2021-11-08 13:28:43 +0000

    ___Updated docs for master___

---
+++ master/implementors/wasmi/imports/trait.ImportResolver.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["sp_sandbox"] = [{"text":"impl&lt;T&gt; ImportResolver for <a class=\"struct\" href=\"sp_sandbox/embedded_executor/struct.EnvironmentDefinitionBuilder.html\" title=\"struct sp_sandbox::embedded_executor::EnvironmentDefinitionBuilder\">EnvironmentDefinitionBuilder</a>&lt;T&gt;","synthetic":false,"types":["sp_sandbox::embedded_executor::EnvironmentDefinitionBuilder"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
