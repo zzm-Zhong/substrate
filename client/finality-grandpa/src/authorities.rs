0d478b967911139228f08b86d43a4ca953d5d2f6
Ming <btxhuman@gmail.com>
2022-03-04 16:04:21 +0800

    Revert "add Serialize derive"
    This reverts commit 836ac0b361be226f1bc21b7f2c74e1671fe29d39.

--- client/finality-grandpa/src/authorities.rs
+++ client/finality-grandpa/src/authorities.rs
@@ -20,7 +20,6 @@
 
 use std::{cmp::Ord, fmt::Debug, ops::Add};
 
-use crate::SetId;
 use finality_grandpa::voter_set::VoterSet;
 use fork_tree::ForkTree;
 use log::debug;
@@ -28,9 +27,9 @@ use parity_scale_codec::{Decode, Encode};
 use parking_lot::MappedMutexGuard;
 use sc_consensus::shared_data::{SharedData, SharedDataLocked};
 use sc_telemetry::{telemetry, TelemetryHandle, CONSENSUS_INFO};
-#[cfg(feature = "std")]
-pub use serde::Serialize;
 use sp_finality_grandpa::{AuthorityId, AuthorityList};
+use sp_runtime::Serialize;
+use crate::SetId;
 
 /// Error type returned on operations on the `AuthoritySet`.
 #[derive(Debug, derive_more::Display)]
@@ -70,8 +69,7 @@ impl<N, E: std::error::Error> From<E> for Error<N, E> {
 }
 
 /// A shared authority set.
-#[cfg_attr(feature = "std", derive(Serialize))]
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 pub struct SharedAuthoritySet<H, N> {
 	inner: SharedData<AuthoritySet<H, N>>,
 }
